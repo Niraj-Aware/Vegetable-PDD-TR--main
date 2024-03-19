52f612b0f830a031fd4abb1d7a187595e58ad405
Niraj-Aware <92052184+Niraj-Aware@users.noreply.github.com>
2024-03-20 00:04:46 +0530

    Update App.py

--- App.py
+++ App.py
@@ -81,14 +81,31 @@ def detect_disease(model, class_names):
             st.write("Unknown disease detected")
         
         st.write("Predicted Class : ", predicted_class, " Confidence Level : ", confidence)
-        st.write("")
-        st.write("To obtain more details and accurate treatment, please visit the nearest pharmaceutical or plant pharma facility")
 
-if selected_option == 'Potato':
-    detect_disease(MODEL, class_names)
-elif selected_option == 'Tomato':
-    detect_disease(TOMATO_MODEL, tomato_classes)
-elif selected_option == 'Pepper':
-    detect_disease(PEPPER_MODEL, pepper_classes)
-else:
-    st.write("Plant not available")
+#Uploading the dog image
+plant_image = st.file_uploader("Choose an image...", type="jpg")
+submit = st.button('Predict')
+
+#On predict button click
+if submit:
+    if plant_image is not None:
+        # Convert the file to an opencv image.
+        file_bytes = np.asarray(bytearray(plant_image.read()), dtype=np.uint8)
+        opencv_image = cv2.imdecode(file_bytes, 1)
+
+        # Displaying the image
+        st.image(opencv_image, channels="BGR")
+        st.write(opencv_image.shape)
+        # Resizing the image
+        opencv_image = cv2.resize(opencv_image, (256,256))
+        # Convert image to 4 Dimension
+        opencv_image.shape = (1,256,256,3)
+        
+        if selected_option == 'Potato':
+            detect_disease(MODEL, class_names)
+        elif selected_option == 'Tomato':
+            detect_disease(TOMATO_MODEL, tomato_classes)
+        elif selected_option == 'Pepper':
+            detect_disease(PEPPER_MODEL, pepper_classes)
+        else:
+            st.write("Plant not available")
