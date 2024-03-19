1c6f768d3aedb56c8fc303dce043daa2c3545b57
Niraj-Aware <92052184+Niraj-Aware@users.noreply.github.com>
2024-03-20 00:41:15 +0530

    Update App.py

--- App.py
+++ App.py
@@ -1,39 +1,50 @@
-# Library imports
+#Library imports
 import numpy as np
 import streamlit as st
 import cv2
 from keras.models import load_model
-import tensorflow as tf
 
-# Loading the Model (Replace './tomato_trained_models/1' with the path to your tomato disease model)
-model = tf.keras.models.load_model('tomato.h5')
+#Loading the Model
+model = load_model('./dataset/disease.h5')
 
-# Setting Title of App
-st.title("Tomato Plant Disease Detection")
-st.markdown("Upload an image of the tomato plant leaf")
+#Name of Classes
+CLASS_NAMES = ['Corn-Common_rust', 'Potato-Early_blight', 'Tomato-Bacterial_spot']
 
-# Uploading the tomato plant leaf image
+#Setting Title of App
+st.title("Plant Disease Detection")
+st.markdown("Upload an image of the plant leaf")
+
+#Uploading the dog image
 plant_image = st.file_uploader("Choose an image...", type="jpg")
-
-# On predict button click
-if st.button('Predict'):
+submit = st.button('Predict')
+#On predict button click
+if submit:
     if plant_image is not None:
-        # Convert the file to an OpenCV image
+        # Convert the file to an OpenCV image.
         file_bytes = np.asarray(bytearray(plant_image.read()), dtype=np.uint8)
         opencv_image = cv2.imdecode(file_bytes, 1)
 
         # Displaying the image
         st.image(opencv_image, channels="BGR")
         st.write(opencv_image.shape)
-
+        
         # Resizing the image
-        opencv_image = cv2.resize(opencv_image, (256, 256))
-
+        opencv_image = cv2.resize(opencv_image, (256,256))
         # Convert image to 4 Dimension
-        opencv_image = np.expand_dims(opencv_image, axis=0)
-
+        opencv_image.shape = (1,256,256,3)
         # Make Prediction
-        predictions = model.predict(opencv_image)
-        predicted_class_index = np.argmax(predictions)
-        predicted_class = model.classes_[predicted_class_index]  # Assuming your model has classes_ attribute
-        st.title(f"This is a tomato leaf with {predicted_class.replace('_', ' ')}")
+        Y_pred = model.predict(opencv_image)
+        result = CLASS_NAMES[np.argmax(Y_pred)]
+        
+        # Displaying disease and recommendations
+        st.header(f"Disease detected: {result.split('-')[0]} leaf with {result.split('-')[1]}")
+        
+        if result == 'Tomato-Bacterial_spot':
+            st.subheader("Recommendations:")
+            st.write("1. Use disease-free seeds or transplants.")
+            st.write("2. Practice crop rotation to avoid soilborne diseases.")
+            st.write("3. Use drip irrigation to avoid wetting the foliage.")
+            st.write("4. Apply copper-based fungicides.")
+            st.write("5. Remove and destroy infected plants promptly.")
+        else:
+            st.write("No recommendations available for this disease.")
